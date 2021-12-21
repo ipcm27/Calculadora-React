5ce3adbd6bacdf04a7f00e7628a1b244f1d19528
Igor Pierre <igorpcmiranda@gmail.com>
2021-12-21 09:14:15 -0300

    adding the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./Main/Calculator";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator></Calculator>
+  </div>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
