880c8e426646def3f932d68af821ab27d5ce02a4
Igor Miranda <igorpcmiranda@gmail.com>
2022-01-09 21:01:17 -0300

    Adicionando lógica ao jogo

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import "./Button.css";
 
 export default (props) => {
+  let classes = "button";
+  classes += props.operation ? " operation" : "";
+  classes += props.double ? " double" : "";
+  classes += props.triple ? " triple" : "";
+
   return (
-    <div>
-      <button className="button">{props.label}</button>
-    </div>
+    <button
+      onClick={(event) => props.click && props.click(props.label)}
+      className={classes}
+    >
+      {props.label}
+    </button>
   );
 };
+
+// Este evento retorna o conteúdo do botão. O propsclick recebe no botão o conteúdo do botão
+// o && faz garantir que só vai entrr na segunda aprte da função se a primeira for válida.
+
+/* O conteúdo de classes = Exibição condicional de classe *ngIf */
